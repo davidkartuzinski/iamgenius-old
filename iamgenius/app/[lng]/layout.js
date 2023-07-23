8fffe9c78f83d0a0c060e91554deb6989fc03f0a
davidkartuzinski <david@kaidawei.me>
2023-07-23 17:35:28 +0200

    add fonts and styled fonts

--- iamgenius/app/[lng]/layout.js
+++ iamgenius/app/[lng]/layout.js
@@ -4,6 +4,13 @@ import '../styles/global.css';
 import { Content } from './components/Content';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
+import {
+  frank_ruhl_libre,
+  source_sans_3,
+  tempo_heavy,
+  tempo_standard,
+  work_sans,
+} from './fonts/fonts';
 
 import styles from './layout.module.css';
 export async function generateStaticParams() {
@@ -12,7 +19,11 @@ export async function generateStaticParams() {
 
 export default function RootLayout({ children, params: { lng } }) {
   return (
-    <html lang={lng} dir={dir(lng)}>
+    <html
+      lang={lng}
+      dir={dir(lng)}
+      className={`${source_sans_3.variable} ${frank_ruhl_libre.variable} ${work_sans.variable} ${tempo_standard.variable} ${tempo_heavy.variable}`}
+    >
       <head />
       <body>
         <div className={styles.wrapper}>
