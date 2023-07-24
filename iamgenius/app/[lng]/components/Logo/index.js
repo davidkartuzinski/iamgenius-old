0b6c2c565264208856f5bff25de18e01b2f21833
davidkartuzinski <david@kaidawei.me>
2023-07-25 00:49:40 +0200

    fixed logo issue

--- iamgenius/app/[lng]/components/Logo/index.js
+++ iamgenius/app/[lng]/components/Logo/index.js
@@ -1,13 +1,13 @@
 import Image from 'next/image';
-import logo from '../Logo/genius-pencil-logo.svg';
+import logo from '../Logo/logo.png';
 
 const Logo = () => {
   return (
     <Image
       priority
       src={logo}
-      width={156.4}
-      height={247.5}
+      width={63.5}
+      height={75}
       alt='I am Genius logo'
     />
   );
